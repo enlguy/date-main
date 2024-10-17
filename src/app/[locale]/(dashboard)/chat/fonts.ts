720d6dff7d703ade973e62656e06ec3467f488a5
Jack <jack282323@gmail.com>
2024-10-17 15:03:53 -0600

    Deleting chat start due to deploy errors

--- src/app/[locale]/(dashboard)/chat/fonts.ts
+++
@@ -1,4 +0,0 @@
-import { Poppins, Roboto } from "next/font/google";
-
-export const poppins = Poppins({ weight: ['400', '500', '600', '700'], subsets: ["latin"] });
-export const roboto = Roboto({weight: ['400', '500', '700'], subsets: ["latin"]});
